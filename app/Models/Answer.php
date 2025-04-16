c5ad259f82e2916c14a1b74e1728b5c09b2c2fbc
Matias Santillan <sntlln.93@gmail.com>
2025-04-16 10:58:32 -0300

    feat: add bkp endpoint

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Answer extends Model
 {
     public function games(): BelongsToMany
     {
         return $this->belongsToMany(Game::class);
+    }
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [WelcomeController::class, 'index'])->name('home');
 Route::get('/game/play', [QuestionController::class, 'index'])->name('game.play');
 Route::post('/game', [GameController::class, 'store'])->name('game.store');
+
+Route::get('bkp/', function() {
+    $games = App\Models\Game::with('answers.question')->get();
+    $questions = App\Models\Question::all();
+
+    return response()->json([
+        'games' => $games,
+        'questions' => $questions,
+    ]);
+});
